575021e69b8c5c795f535993ee6cb9257466279d
mohayax <mohayax02@gmail.com>
2022-09-25 03:06:53 +0100

    additions made

--- app.js
+++ app.js
@@ -8,3 +8,21 @@ toggleBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
     navlinks.classList.toggle("show-links");
 });
+
+// sinlge product image selector
+
+const mainIMg = document.getElementById('main-img');
+const sampleImg = document.querySelectorAll('.sample-img');
+const imgG = document.querySelector('.img-group');
+const imgC = document.querySelectorAll('.imgClass');
+
+sampleImg.forEach((image, i) => image.addEventListener('click', function () {
+    mainIMg.src = imgC[i].src;
+    
+}))
+
+// click on shop page product to open single product page
+
+const product = document.querySelectorAll('.pro');
+
+product.forEach((pro) => pro.addEventListener('click', () => window.location.href="single.html"))
